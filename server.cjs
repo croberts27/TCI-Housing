b778ff2731a28461964cf1d42f4ecfaaefd69b09
Calvin Roberts <you@example.com>
2024-01-04 14:58:27 -0700

    added post reqauest to server.cjs

--- server.cjs
+++ server.cjs
@@ -38,6 +38,24 @@ transporter.sendMail(mailOptions, function (err, data) {
   }
 });
 
+app.post("/send", function (req, res) {
+  let mailOptions = {
+    from: "test@gmail.com",
+    to: process.env.EMAIL,
+    subject: "Nodemailer API",
+    text: "Hi from your nodemailer API",
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+      res.json({ status: "Email sent" });
+    }
+  });
+});
+
 const port = 3001;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
