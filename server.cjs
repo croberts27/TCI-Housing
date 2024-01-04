e5aa4fef5970bdee1802bd4b580d8d891633a04c
Calvin Roberts <you@example.com>
2024-01-04 14:46:24 -0700

    server.cjs updates. nodemailerapi runs successfully

--- server.cjs
+++ server.cjs
@@ -1,13 +1,9 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
 require("dotenv").config();
-
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
@@ -19,4 +15,30 @@ let transporter = nodemailer.createTransport({
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
   },
+});
+
+transporter.verify((err, success) => {
+  err
+    ? console.log(err)
+    : console.log(`=== Server is ready to take messages: ${success} ===`);
+});
+
+let mailOptions = {
+  from: "test@gmail.com",
+  to: process.env.EMAIL,
+  subject: "Nodemailer API",
+  text: "Hi from your nodemailer API",
+};
+
+transporter.sendMail(mailOptions, function (err, data) {
+  if (err) {
+    console.log("Error " + err);
+  } else {
+    console.log("Email sent successfully");
+  }
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
